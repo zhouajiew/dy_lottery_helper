28a0ad3cbbf511b6dc698638d36f2a854ac9c396
摆烂Jo <32508140+zhouajiew@users.noreply.github.com>
2025-09-23 02:35:37 +0800

    Create edge_automation.py

---
+++ useful_code/edge_automation.py
@@ -0,0 +1,60 @@
+# 自动化下的Edge浏览器推荐设置
+import json
+import os
+
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+
+# 获取当前文件所在的目录
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 构建相对路径
+relative_path = os.path.join(current_dir)
+
+# 获取日志
+def get_driver_logs():
+    logs = driver.get_log("performance")
+
+    for log in logs:
+        logjson = json.loads(log["message"])["message"]
+        # 获取Network日志
+        if logjson['method'] == 'Network.responseReceived':
+            params = logjson['params']
+            requestUrl = params['response']
+
+if __name__ == "__main__":
+    # 设置用户数据目录的路径
+    user_data_dir = f'{relative_path}/user/data/dir'
+
+    edge_options = webdriver.EdgeOptions()
+    # 保持登录会话信息
+    edge_options.add_argument(f'--user-data-dir={user_data_dir}')
+    # 无头模式
+    # edge_options.add_argument('--headless')
+
+    # 屏蔽inforbar
+    edge_options.add_experimental_option('useAutomationExtension', False)
+    edge_options.add_experimental_option('excludeSwitches', ['enable-automation', 'enable-logging'])
+
+    service = Service(executable_path=f'{relative_path}/msedgedriver.exe')
+    # 开启performance，记录日志
+    edge_options.set_capability("ms:loggingPrefs", {'performance': 'ALL'})
+
+    driver = webdriver.Edge(options=edge_options)
+    # 浏览器大小
+    driver.set_window_size(700, 600)
+    # 浏览器位置
+    driver.set_window_position(50, 200)
+
+    # 设置超时时间
+    driver.set_page_load_timeout(20)
+
+    # 浏览器执行stealth.min.js文件里的代码以应对多数情况的反反爬
+    with open(f"{relative_path}/stealth.min.js") as f:
+        js = f.read()
+    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+        "source": js
+    })
+
+    # 打开抖音官网
+    driver.get('https://live.douyin.com/categorynew/4_103')
